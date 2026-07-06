1f0f4481970b12ac16fda8b35291303e3ba0a4a4
Allan Sechrist <sechristallan@gmail.com>
2026-07-06 20:17:54 +0900

    add rotation controls to ship.

--- scenes/ship/ship.gd
+++ scenes/ship/ship.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 class_name Ship
 
-@onready var thruster_animation: AnimatedSprite2D = $ThrusterAnimation
+@export var turn_speed := 0.05
+@export var thrust_speed := 50.0
+@export var drag := 5.0 
 
+@onready var thruster_animation: AnimatedSprite2D = $ThrusterAnimation
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("move"):
 		thruster_animation.play("Thrust")
 	else:
 		thruster_animation.play("Idle")
+	
+	if Input.is_action_pressed("turn_left"):
+		rotate(-turn_speed)
+	if Input.is_action_pressed("turn_right"):
+		rotate(turn_speed)
