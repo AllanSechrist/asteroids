64928c625591fee614ab504b568184e6a48f8603
Allan Sechrist <sechristallan@gmail.com>
2026-07-06 20:10:16 +0900

    add thrust animation to ship

--- scenes/ship/ship.gd
+++ scenes/ship/ship.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 class_name Ship
 
+@onready var thruster_animation: AnimatedSprite2D = $ThrusterAnimation
+
+
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("move"):
-		print("moving")
-		
+		thruster_animation.play("Thrust")
+	else:
+		thruster_animation.play("Idle")
