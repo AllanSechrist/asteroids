f551aca916c30134ca9102f68be34c9dbac589d2
Allan Sechrist <sechristallan@gmail.com>
2026-07-14 22:00:35 +0900

    changes lives from text to icons. Finish UI HUD.

--- scenes/UI/ui.gd
+++ scenes/UI/ui.gd
@@ -1,11 +1,12 @@
 extends CanvasLayer
 class_name UI
 
-@onready var lives_label: Label = %LivesLabel
+#@onready var lives_label: Label = %LivesLabel
 @onready var score_label: Label = %ScoreLabel
 @onready var high_label: Label = %HighLabel
+@onready var lives_container: HBoxContainer = %LivesContainer
 
-
+@export var life_icon_scene: PackedScene
 
 func _ready() -> void:
 	var game_manager := get_parent() as GameManager
@@ -21,7 +22,11 @@ func _on_score_changed(new_score: int) -> void:
 	score_label.text = "SCORE: %d" % new_score
 	
 func _on_lives_changed(new_lives: int) -> void:
-	lives_label.text = "LIVES: %d" % new_lives
+	for child in lives_container.get_children():
+		child.queue_free()
+	for i in new_lives:
+		var icon = life_icon_scene.instantiate()
+		lives_container.add_child.call_deferred(icon)
 	
 func _on_game_over() -> void:
 	pass
