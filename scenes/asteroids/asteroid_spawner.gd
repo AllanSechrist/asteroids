3b3e431251a2a914830a53c6b968ab279d379e44
Allan Sechrist <sechristallan@gmail.com>
2026-07-19 12:53:50 +0900

    refactor asteroid spawner

--- scenes/asteroids/asteroid_spawner.gd
+++ scenes/asteroids/asteroid_spawner.gd
@@ -1,12 +1,6 @@
 extends Node2D
 class_name AsteroidSpawner
 
-@onready var top: Marker2D = $Top
-@onready var right: Marker2D = $Right
-@onready var bottom: Marker2D = $Bottom
-@onready var left: Marker2D = $Left
-@onready var positions := [top, right, bottom, left]
-
 @export var asteroid_scene: PackedScene
 
 var asteroids: int
@@ -16,13 +10,21 @@ signal asteroid_score(score: int)
 
 func spawn_asteroids(amount: int) -> void:
 	for i in amount:
-		var variance := Vector2(randf_range(1,10), randf_range(1,10))
 		var asteroid := asteroid_scene.instantiate()
-		var spawn_position = positions.pick_random()
-		asteroid.global_position = spawn_position.global_position
+		asteroid.global_position = get_random_edge_position()
 		asteroid.destroyed.connect(_on_asteroid_destroyed)
 		add_child.call_deferred(asteroid)
 		asteroids += 1
+		
+func get_random_edge_position() -> Vector2:
+	var screen_size := get_viewport_rect().size
+	var edge := randi() % 4
+	
+	match edge:
+		0: return Vector2(randf_range(0, screen_size.x), 0)
+		1: return Vector2(screen_size.x, randf_range(0,screen_size.y))
+		2: return Vector2(randf_range(0, screen_size.x), screen_size.y)
+		_: return Vector2(0, randf_range(0, screen_size.y))
 	
 func _on_asteroid_destroyed(asteroid: Area2D) -> void:
 	
